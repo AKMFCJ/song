7fb98f636002fecf326124afac7a3b738aaa70d5
roy <AKM_FAN@163.com>
2015-06-10 21:40:35 +0800

    django admin action

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,15 +3,20 @@ from myapp.models import Article
 # Register your models here.
 
 
-def make_published(modeladmin, request, queryset):
-	print dir(modeladmin)
-	queryset.update(status='p')
-make_published.short_description = "Mark selected stories as published"
-
-
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ['title', 'status']
-	ordering = ['title']
-	actions = [make_published]
+    list_display = ['title', 'status']
+    ordering = ['title']
+    actions = ['make_published', 'export_selectd_objects']
+    
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='p')
+        if rows_updated == 1:
+            message_bit = "1 story was"
+        else:
+            message_bit = "%s stories were" % rows_updated
+
+        self.message_user(request, "%s successfully maked as published." % message_bit)
+
+    make_published.short_description = "Mark selected stories as published"
 
 admin.site.register(Article, ArticleAdmin)
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
+#-*- coding:utf-8 -*-
 from django.shortcuts import render
+from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def export_selected_objects(modeladmin, request, queryset):
+    selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+    ct = ContentType.objects.get_for_model(queryset.model)
+
+    return HttpResponseRedirct("/export/?ct=%sids=%s" % (ct.pk, ",".join(selected)))
+
+
+admin.site.add_action(export_selectd_objects)
